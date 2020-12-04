13a717f183a995798212b8592b200db8d25b0ab4
sknrchoi <seulkinara@gmail.com>
2020-12-04 13:35:32 +0900

    #1.1 Refactoring function and Extrating jobs page

--- job-scrapper/main.py
+++ job-scrapper/main.py
@@ -1,24 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
+last_indeed_pages = extract_indeed_pages()
 
-indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
-
-pagination = indeed_soup.find("div", {"class":"pagination"})
-
-#list
-links = pagination.find_all('a')
-pages = []
-# except the last one
-for link in links[:-1]:
-  pages.append(int(link.string))
-
-# first item from the end
-#print(pages[-1])
-
-# get maximum number
-max_page = pages[-1]
-
-for n in range(max_page):
-  print(f"start={n * 50}")
+indeed_jobs = extract_indeed_jobs(last_indeed_pages)
