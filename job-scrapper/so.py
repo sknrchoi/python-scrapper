1d8478b711b521bc2d210acf938803734a49d840
sknrchoi <seulkinara@gmail.com>
2020-12-06 19:03:39 +0900

    #1.6 StackOverflow extract job ids

--- job-scrapper/so.py
+++ job-scrapper/so.py
@@ -1,16 +1,67 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = f"https://stackoverflow.com/jobs?q=python&sort=i"
+LIMIT = 50
+URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 
 
 def get_last_page():
     result = requests.get(URL)
-    soup = BeautifulSoup(result.text, "html.parser")
-    pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
-    print(pages)
+    soup = BeautifulSoup(result.text, 'html.parser')
+    pagination = soup.find("div", {"class": "pagination"})
+
+    #list
+    links = pagination.find_all('a')
+    pages = []
+    # except the last one
+    for link in links[:-1]:
+        pages.append(int(link.string))
+
+    # first item from the end
+    #print(pages[-1])
+
+    # get maximum number
+    max_page = pages[-1]
+    return max_page
+
+
+def extract_job(html):
+    title = html.find("h2", {"class": "title"}).find("a")["title"]
+    company = html.find("span", {"class": "company"})
+    if company:
+      company_anchor = company.find("a")
+      if company_anchor is not None:
+          company = str(company_anchor.string)
+      else:
+          company = str(company.string)
+      company = company.strip()
+    else:
+      company = None
+    location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
+    job_id = html["data-jk"]
+    return {
+        'title': title,
+        'company': company,
+        'location': location,
+        'link': f"https://www.indeed.com/viewjob?jk={job_id}"
+    }
+
+
+def extract_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        print(f"Scrapping page {page}")
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+
+    for result in results:
+        job = extract_job(result)
+        jobs.append(job)
+    return jobs
 
 
 def get_jobs():
-    last_page = get_last_page()
-    return []
+    last_pages = get_last_page()
+    jobs = extract_jobs(last_pages)
+    return jobs
