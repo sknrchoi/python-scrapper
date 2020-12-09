5b36e4d09147eb5982d54ba3a6a8faa5ba9b5858
sknrchoi <seulkinara@gmail.com>
2020-12-09 22:32:37 +0900

    #3.2 Forms and Query Arguments

--- super-scrapper/main.py
+++ super-scrapper/main.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-app = Flask("Super-Scrapper")
+app = Flask("SuperScrapper")
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/report")
+def report():
+    word = request.args.get('word')
+    return render_template("report.html", searchingBy=word)
 
 app.run()
