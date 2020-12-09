44119a77c613d8ca54fadc631c1c756223ff4985
sknrchoi <seulkinara@gmail.com>
2020-12-09 22:57:21 +0900

    #3.3 Add Job Scrapper from job-scrapper

--- super-scrapper/main.py
+++ super-scrapper/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from scrapper import get_jobs
 
 app = Flask("SuperScrapper")
 
@@ -10,6 +11,12 @@ def home():
 @app.route("/report")
 def report():
     word = request.args.get('word')
+    if word:
+        word = word.lower()
+        jobs = get_jobs(word)
+        print(jobs)
+    else:
+        return redirect("/")
     return render_template("report.html", searchingBy=word)
 
 app.run()
