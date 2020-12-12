b16a29e89e22b0c7b031401c45bb991e0e4af87f
sknrchoi <seulkinara@gmail.com>
2020-12-12 12:35:04 +0900

    #3.5 Rendering jobs using css grid attribute

--- super-scrapper/main.py
+++ super-scrapper/main.py
@@ -14,15 +14,14 @@ def report():
     word = request.args.get('word')
     if word:
         word = word.lower()
-        fromDB = db.get(word)
-        if fromDB:
-            jobs = fromDB
+        existingJobs = db.get(word)
+        if existingJobs:
+            jobs = existingJobs
         else:
             jobs = get_jobs(word)
             db[word] = jobs
-        print(jobs)
     else:
         return redirect("/")
-    return render_template("report.html", searchingBy=word, resultsNumber=len(jobs))
+    return render_template("report.html", searchingBy=word, resultsNumber=len(jobs), jobs=jobs)
 
 app.run()
